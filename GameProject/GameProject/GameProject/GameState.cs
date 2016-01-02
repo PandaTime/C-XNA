3ffffdb6f9e2ea5e6eaa9cd71d8a4cf9e4014ac6
Yaroslav Stasiuk <Yaroslav Stasiuk>
2016-01-02 22:17:18 +0200

    XNA

---
+++ GameProject/GameProject/GameProject/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameProject
+{
+    /// <summary>
+    /// An enumeration of the possible game states
+    /// </summary>
+    public enum GameState
+    {
+        Game,
+        Result
+    }
+}
